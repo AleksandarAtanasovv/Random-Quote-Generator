c898fc4beb094fc0eea750008027b4a8bebcbcf1
Sakinaa782 <sashotrupa@hotmail.com>
2022-08-09 21:36:50 +0300

    Better styling and animations

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ async function getQuote(){
   const data = await fetch("https://goquotes-api.herokuapp.com/api/v1/random?count=1")
   const respData = await data.json();
   console.log(respData.quotes[0].text);
+  
   p.textContent = respData.quotes[0].text;
   author.textContent = respData.quotes[0].author
 }
